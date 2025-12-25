dc4f2efbb035f28d20a67ae50870a1d10517864a
googxho <gxh522@qq.com>
2025-12-26 00:04:17 +0800

    update

--- auth.config.ts
+++ auth.config.ts
@@ -3,7 +3,7 @@ import type { NextAuthConfig } from 'next-auth';
 export const authConfig = {
   // Secret used by Auth.js/NextAuth for signing tokens. Prefer setting
   // NEXTAUTH_SECRET in environment for production.
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.AUTH_SECRET || 's3cr3t_xJkM2s9fPqL1vZ0aH8',
   providers: [],
   pages: {
     signIn: '/login',
